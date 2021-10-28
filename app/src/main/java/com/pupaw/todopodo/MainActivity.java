74f059b0867a7f0c0f2d61c6658a3fd613c96853
pup-paw <wldusdl0310@gmail.com>
2021-10-29 02:23:01 +0900

    design the main page(incomplete)

---
+++ app/src/main/java/com/pupaw/todopodo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pupaw.todopodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
